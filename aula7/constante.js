9a8fcc0e20dd1a447dd6c5c2adddec0fed0fe2a6
Guilherme Bueno <gui.guimatheus@hotmail.com>
2025-07-31 20:59:14 -0300

    update

---
+++ aula7/constante.js
@@ -0,0 +1,14 @@
+// const nao pode ser definida e não ser utilizada.
+// const é uma variavel constante como diz o nome, e nao pode ser alterada.  
+
+const nome = 'João';
+//nome = 'Fernano';  = TypeError: Assignment to constant variable.
+console.log(nome);
+
+
+const primeiroNumero = 10;
+const segundoNumero = 5;
+const resultSomaConta = primeiroNumero * segundoNumero;
+console.log(resultSomaConta);
+const resultadoDuplicado = resultSomaConta * 2;
+console.log(resultadoDuplicado);
---
+++ aula8/exercicio.js
@@ -0,0 +1,20 @@
+/*
+Luiz Otávio Miranda tem 30 anos, pesa 84 kg
+tem 1.8 de altura e seu IMC é de 25.925925925925924
+Luiz Otávio nasceu em 1980
+*/
+let anoAtual = new Date().getFullYear();
+const nome = 'Guilherme'
+const sobrenome = 'Bueno'
+const idade = 25;
+const peso = 78;
+const altura = 1.76;
+let imc;
+let anoNascimento = 2000
+
+let calculaIdade = anoAtual - anoNascimento;
+imc =  peso / (altura * altura);
+
+
+console.log(nome, sobrenome, 'possui', calculaIdade, 'anos', ', pesa', peso, 'kg')
+console.log('possui', altura, 'e seu IMC é de', imc)
