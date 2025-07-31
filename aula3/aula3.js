de824eecc31a297c00e9b606bd72079b331c9642
Guilherme Bueno <gui.guimatheus@hotmail.com>
2025-07-31 19:00:08 -0300

    update

---
+++ aula3/aula3.js
@@ -0,0 +1,15 @@
+console.log('Guilherme Bueno');   //String
+console.log("Guilherme Bueno 2");  //
+console.log(`'Guilherme' 'Bueno' "Teste" 3`);  
+
+
+console.log(12345);  //Number
+console.log(10.5);  //Number
+
+console.log(1234, 10.5, "Guilherme Bueno", "Texto"); 
+
+
+
+//Exercicio. 
+
+console.log('Meu nome é Guilherme.', 'Estou aprendendo JavaScript às', 18, 'da tarde')
