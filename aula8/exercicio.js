aedc6301387bd3caf1485a249bf1168a58aa780c
Guilherme Bueno <gui.guimatheus@hotmail.com>
2025-08-04 22:35:24 -0300

    update

--- aula8/exercicio.js
+++ aula8/exercicio.js
@@ -3,6 +3,7 @@ Luiz Otávio Miranda tem 30 anos, pesa 84 kg
 tem 1.8 de altura e seu IMC é de 25.925925925925924
 Luiz Otávio nasceu em 1980
 */
+
 let anoAtual = new Date().getFullYear();
 const nome = 'Guilherme'
 const sobrenome = 'Bueno'
@@ -18,3 +19,5 @@ imc =  peso / (altura * altura);
 
 console.log(nome, sobrenome, 'possui', calculaIdade, 'anos', ', pesa', peso, 'kg')
 console.log('possui', altura, 'e seu IMC é de', imc)
+
+console.log('teste')
