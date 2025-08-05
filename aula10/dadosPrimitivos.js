68c190666afc8854d0c9db58b46dd95ce8e1f69a
Guilherme Bueno <gui.guimatheus@hotmail.com>
2025-08-04 23:16:41 -0300

    update

---
+++ aula10/dadosPrimitivos.js
@@ -0,0 +1,9 @@
+//Ja vimos, String, number, undefined. 
+
+const nome = 'Guilherme';  //Usando ''
+const nome1 = "Guilherme1"   // " "
+const nome2 = `Guilherme1`   // `  `
+
+
+
+
---
+++ aula9/exercicio.js
@@ -0,0 +1,8 @@
+/*
+Diferenças entre VAR e LET 
+*/
+
+let nome = 'Guilherme' //Nao pode ser redeclarado. 
+
+var nome2 = 'Guilherme' //Desa forma eu posso redeclarar a variavel. 
+var nome2 = 'Guilherme2'
