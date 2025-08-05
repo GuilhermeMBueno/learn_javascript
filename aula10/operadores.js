362bb136685109e831d8e27f616198de6868b968
Guilherme Bueno <gui.guimatheus@hotmail.com>
2025-08-05 13:02:42 -0300

    update

---
+++ aula10/operadores.js
@@ -0,0 +1,14 @@
+/* --- Operadores Aritmeticos ---
++ Adicao ou Concatenacao.
+** Potencia
+% Resto da Divisao
+*/  
+
+
+const num1 = 5;
+const num2 = 10;
+console.log(num1 + num2);
+
+const num3 = '5';
+const num4 = 10;
+console.log(num3 + num4);
