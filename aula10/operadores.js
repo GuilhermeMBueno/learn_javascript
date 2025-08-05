37fc38b4960d4b41e8946bb63cbb92e0f09b06d2
Guilherme Bueno <gui.guimatheus@hotmail.com>
2025-08-05 13:27:26 -0300

    update

--- aula10/operadores.js
+++ aula10/operadores.js
@@ -12,3 +12,11 @@ console.log(num1 + num2);
 const num3 = '5';
 const num4 = 10;
 console.log(num3 + num4);
+
+const num5 = 3
+const num6 = 7
+console.log(num5 % num6)
+
+console.log(num1 + num2 + num1)
+console.log(num1 + num2 * num3)
+console.log((num1 + num2) * num3)
