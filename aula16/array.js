0068e175f03bb19e719b7d8817becf4bd91b3ed9
Guilherme Bueno <gui.guimatheus@hotmail.com>
2025-08-14 21:36:20 -0300

    update

---
+++ aula16/array.js
@@ -0,0 +1,29 @@
+//            123456789......
+const nome = 'Guilherme Bueno'
+const alunos = 'Luiz Naria João' //Pode ser um Array, mas é muito complexo manipular.
+console.log(nome[2]);  // Retornaria =  i
+//               0     1      2
+const alunos2 = ['Luiz', 'Maria', 'João']; //Maneira Correta.
+
+alunos2.unshift('Teste1Valor');
+alunos2.unshift('Teste-1Valor');
+console.log(alunos2)
+
+                   
+
+/*
+alunos2[0] = 'Guilherme'
+alunos2[3] = 'Fernando'
+alunos2[4] = 'Tenis'
+console.log(alunos2);
+console.log(alunos2[0]);
+console.log(alunos2[1]);
+console.log(alunos2.length)
+alunos2[alunos2.length] = 'Jeferson'
+alunos2[alunos2.length] = 'Veronica'
+alunos2[alunos2.length] = 'Rafael'
+alunos2[alunos2.length] = 'Otavio' //Adiciona valores a Array
+console.log(alunos2.length)
+alunos2.push('Mariana'); // Adiciona valores ao final do Array
+console.log(alunos2);
+*/
