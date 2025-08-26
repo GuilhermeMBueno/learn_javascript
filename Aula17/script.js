c3b190f24212f44974c6d69b74a55fd9da81fd59
Guilherme Bueno <gui.guimatheus@hotmail.com>
2025-08-25 22:02:25 -0300

    update

---
+++ Aula17/script.js
@@ -0,0 +1,53 @@
+/*
+function saudacao(nome) {
+    console.log(`Bom dia ${nome}!`);
+}
+
+const variavel = saudacao('Guilherme');
+console.log(variavel)
+
+
+function soma (x, y){
+    const resultado = x + y;
+    return resultado;
+}
+console.log(soma(2,5))
+console.log(soma(2,4))
+console.log(soma(2,6))
+
+
+function soma(x, y) {
+    const resultado = x + y;
+    return resultado;
+}
+
+
+const raiz = function (n) {
+ return n ** 0.5;
+};
+
+console.log(raiz(9));
+console.log(raiz(16));
+console.log(raiz(25));
+
+
+
+
+
+const raiz = (n) => {
+    return n ** 0.5;
+};
+console.log(raiz(9));
+console.log(raiz(16));
+console.log(raiz(25));
+ 
+*/
+
+
+
+
+const raiz = n => n ** 0.5;
+console.log(raiz(9));
+console.log(raiz(16));
+console.log(raiz(25));
+ 
---
+++ Aula18/script.js
@@ -0,0 +1,16 @@
+
+//Pode ser alterado os valores dentro de Array.
+const array = [1,2,3];
+//Só nao posso usar Array para alterar o valor dele, quando ele é o CONST. 
+array.push(4);
+array[0] = 'Luiz';
+console.log(array)
+
+
+const nome01 = 'Guilherme';
+const sobreNome01 = 'Bueno';
+const idade01 = 25;
+
+const nome02 = 'Matheus';
+const sobreNome02= 'Nascimento';
+const idade02= 18;
